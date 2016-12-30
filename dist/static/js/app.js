7cdffb0dacb725420c4ef0fab1ab80aa2a96ff77
Brian Quach <13rianquach@gmail.com>
2016-12-29 17:26:07 -0800

    Refact: seperate zomato model from controller

--- dist/static/js/app.js
+++ dist/static/js/app.js
@@ -1,11 +1,11 @@
-var zomatoAPIController = (function ($) {
+var zomatoModel = (function($) {
   'use strict'
 
   var headers = {
     "user-key": '3dcc424e9b2d38abdfd2d55ae0c36d06'
   };
 
-  function init() {
+  function getCafes() {
     var data = $.param({
       lat: mapState.lat,
       lon: mapState.lng,
@@ -15,35 +15,48 @@ var zomatoAPIController = (function ($) {
     });
     var url = 'https://developers.zomato.com/api/v2.1/search';
 
-    $.ajax({
+    return $.ajax({
       url: url,
       data: data,
       headers: headers
-    }).done(function (data) {
-      var location, lat, lng, marker;
-      var bounds = map.getBounds();
-      var restaurants = data.restaurants;
-
-      restaurants.forEach(function (r) {
-        location = r.restaurant.location;
-        lat = location.latitude;
-        lng = location.longitude;
-        if (typeof lat !== 'Number') {
-          lat = Number(lat);
-        }
-        if (typeof lng !== 'Number') {
-          lng = Number(lng);
-        }
-        if (lat !== 0 && lng !== 0) {
-          marker = mapAPIController.addMarker(lat, lng);
-          bounds.extend(marker.getPosition());
-        }
-      });
-      map.fitBounds(bounds);
-    }).fail(function (jqXHR, textStatus, errorThrown) {
-      // TODO: Handle failures
-      // console.log(jqXHR, textStatus, errorThrown);
     });
+  }
+
+  return {
+    getCafes: getCafes
+  }
+})(jQuery);
+
+var zomatoController = (function ($) {
+  'use strict'
+
+  function init() {
+    zomatoModel.getCafes()
+      .done(function (data) {
+        var location, lat, lng, marker;
+        var bounds = map.getBounds();
+        var restaurants = data.restaurants;
+
+        restaurants.forEach(function (r) {
+          location = r.restaurant.location;
+          lat = location.latitude;
+          lng = location.longitude;
+          if (typeof lat !== 'Number') {
+            lat = Number(lat);
+          }
+          if (typeof lng !== 'Number') {
+            lng = Number(lng);
+          }
+          if (lat !== 0 && lng !== 0) {
+            marker = mapAPIController.addMarker(lat, lng);
+            bounds.extend(marker.getPosition());
+          }
+        });
+        map.fitBounds(bounds);
+      }).fail(function (jqXHR, textStatus, errorThrown) {
+        // TODO: Handle failures
+        // console.log(jqXHR, textStatus, errorThrown);
+      });
   }
 
   return {
@@ -71,4 +84,4 @@ var mapAPIController = (function($) {
   };
 })(jQuery);
 
-zomatoAPIController.init();
+zomatoController.init();
